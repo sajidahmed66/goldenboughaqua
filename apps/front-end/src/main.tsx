87109500f53f08a89bac7178ed381391f8ae0aee
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2025-04-13 14:30:00 +0600

    feat: update routing structure and dependencies
    - Added a new router file for managing routes.
    - Updated main.tsx to use react-router for routing instead of react-router-dom.
    - Defined routes for home, about, contact, products, and product reviews.
    - Changed dependency from react-router-dom to react-router version 7.5.0.
    - Updated TypeScript configuration to include path aliases for better module resolution.

--- apps/front-end/src/main.tsx
+++ apps/front-end/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route,  Routes } from 'react-router';
 import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import App from '@front-end/src/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +10,14 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+     <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<div>this is the about page</div>} />
+        <Route path="/contact" element={<div>this is the contact page</div>} />
+        <Route path="/products" element={<div>this is the products page</div>} />
+        <Route path="/products/:id" element={<div>this is the product page</div>} />
+        <Route path="/products/:id/reviews" element={<div>this is the product reviews page</div>} />
+     </Routes>
     </BrowserRouter>
   </StrictMode>
 );
